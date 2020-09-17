97548440c5488ae691e8160086246a637da887e1
Rez5th <69373806+Rez5th@users.noreply.github.com>
2020-09-17 23:03:47 +1000

    V 0.2 fsfsffgs App Added.

--- Component/App.jsx
+++ Component/App.jsx
@@ -1,16 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import UpPage from "../Component/UpPage.jsx";
-import App from "../Component/App.jsx";
+import UpPage from "./UpPage";
 
 import jquery from "jquery";
 import bootstrap from "bootstrap";
 //import Jumbotron from "react-bootstrap/Jumbotron";
-
+//import Toast from "react-bootstrap/Toast";
 //import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
-//import "../public/styles.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function app() {
+  return (
+    <div>
+      <UpPage />
+    </div>
+  );
+}
+
+export default app;
--- Component/UpPage.jsx
+++ Component/UpPage.jsx
@@ -5,7 +5,7 @@ import jquery from "jquery";
 import bootstrap from "bootstrap";
 //import Jumbotron from "react-bootstrap/Jumbotron";
 //import Toast from "react-bootstrap/Toast";
-import Container from "react-bootstrap/Container";
+//import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 import Image from "react-bootstrap/Image";
 import "bootstrap/dist/css/bootstrap.min.css";
